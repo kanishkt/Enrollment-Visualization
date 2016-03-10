ac0d880a58744713c64a1054c94ffcac060cbbc7
Jennifer Cheng <jrcheng3@illinois.edu>
2016-03-09 18:44:51 -0600

    adding basic html for illinois & world, paths broken :(

--- Design 2.py
+++ Design 2.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def index():
     return render_template('us.html')
 
+@app.route('/us')
+def us():
+	return render_template('us.html')
+
+@app.route('/world')
+def world():
+	return render_template('world.html')
+
+@app.route('/illinois')
+def illinois():
+	return render_template('illinois.html')
+
 if __name__ == '__main__':
     app.run()
