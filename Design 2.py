f81d15fce738da97bd51089d1b026d56564fdecd
Kanishk Thareja <kanishkt@gmail.com>
2016-03-10 22:38:25 -0600

    Web Stuff and data cleaning

--- Design 2.py
+++ Design 2.py
@@ -2,21 +2,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('us.html')
 
+
 @app.route('/us')
 def us():
-	return render_template('us.html')
+    return render_template('us.html')
+
 
 @app.route('/world')
 def world():
-	return render_template('world.html')
+    return render_template('world.html')
+
 
 @app.route('/illinois')
 def illinois():
-	return render_template('illinois.html')
+    return render_template('illinois.html')
+
 
 if __name__ == '__main__':
     app.run()
